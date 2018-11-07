c09b3eb84ee226fcd5c3341d78274725b7103118
Jonathan Martz <git@jmartz.de>
2018-11-07 22:25:25 +0100

    add logic for add function

--- controller/TodoController.php
+++ controller/TodoController.php
@@ -16,15 +16,65 @@
 		public function select(Request $request){
 			$validation = $this->validate($request, []);
 
-			$user = $request->user();
-
 			$todos = DB::connection('mysql.read')
 					   ->table('todos')
-					   ->where('id','=',$user->getAuthIdentifier())
+					   ->where('UID','=',$request->user()->getAuthIdentifier())
 					   ->get();
 
 			$this->addResult('todos',$todos);
 			$this->addMessage('success','All your Todos.');
+
+			return $this->getResponse();
+		}
+
+		/**
+		 * @param  Request  $request
+		 * @return Response
+		 */
+		public function add(Request $request){
+			$validation = $this->validate($request, [
+				'todo' => 'bail|required|array',
+				'todo.category' => 'bail|required|string',
+				'todo.title' => 'bail|required|string',
+				'todo.deadline' => 'integer',
+				'todo.description' => 'bail|required|string',
+				'todo.prio' => 'bail|required|alpha',
+			]);
+
+			$todo = $request->input('todo');
+
+			$todos = DB::connection('mysql.read')
+					   ->table('todos')
+					   ->where('title','=',$todo['title'])
+					   ->where('description','=',$todo['description'])
+					   ->where('category','=',$todo['category'])
+					   ->where('UID','=',$request->user()->getAuthIdentifier());
+
+			$count = $todos->count();
+			$existing = $todos->first();
+
+			if($count !== 0){
+				$this->addMessage('error','Todo already exists('.$existing->id.').');
+			}
+			else{
+				$result =  DB::connection('mysql.read')
+							 ->table('todos')
+							 ->insert([
+								 'title'=>$todo['title'],
+								 'category'=>$todo['category'],
+								 'deadline'=>$todo['deadline'],
+								 'description'=>$todo['description'],
+								 'prio'=> $todo['prio'],
+								 'UID' =>$request->user()->getAuthIdentifier()
+							 ]);
+
+				if($result){
+					$this->addMessage('success','All your Todos.');
+				}
+				else{
+					$this->addMessage('warning','Upps da ist wohl was schief gelaufen.');
+				}
+			}
 
 			return $this->getResponse();
 		}
