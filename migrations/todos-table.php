7869af46e5fa060c90dd97e07a4c9667b7d9f2a8
jonathan <bitbucket@jmartz.de>
2019-10-04 11:23:10 +0200

    add migration for todos table

---
+++ migrations/todos-table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('prio');
+            $table->string('status')->default('open');
+            $table->string('category');
+            $table->integer('deadline');
+            $table->integer('UID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
