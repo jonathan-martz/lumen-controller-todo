a17c40b14419d82e453b3a46f10ef0e94d31bee5
jonathan-martz <github@jmartz.de>
2019-12-29 22:37:16 +0100

    add requreiment for auth middelware and update routes

--- routes/web.php
+++ routes/web.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Route;
  * @todo replacec delete with delete call and so on
  */
 Route::post('/user/todo/autocomplete', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@autocomplete'
 ]);
 
 Route::post('/user/todo/delete', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@delete'
 ]);
 
 Route::post('/user/todo/edit', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@edit'
 ]);
 
 Route::post('/user/todo/add', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@add'
 ]);
 
 Route::post('/user/todo/view', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@view'
 ]);
 
 Route::post('/user/todo/select', [
-    'middleware' => ['xss', 'https'],
+    'middleware' => ['auth', 'xss', 'https'],
     'uses' => 'App\Http\Controllers\TodoController@select'
 ]);
