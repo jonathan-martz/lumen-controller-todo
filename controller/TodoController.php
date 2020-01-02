1d1e36b3fed168e20cec92c5322611477ecfee8b
jonathan-martz <github@jmartz.de>
2020-01-02 18:53:53 +0100

    update lumen-framework to 6.3

--- controller/TodoController.php
+++ controller/TodoController.php
@@ -2,61 +2,79 @@
 
 namespace App\Http\Controllers;
 
-use \http\Env\Response;
-use \Illuminate\Http\Request;
-use \Illuminate\Support\Facades\DB;
-use \Illuminate\Support\Facades\Hash;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
+/**
+ * Class TodoController
+ * @package App\Http\Controllers
+ */
 class TodoController extends Controller
 {
-    public function filter($connection, $filters){
-        foreach($filters as $key => $filter){
-            if(count($filter) === 3){
-                $connection->where($filter[0],$filter[1],$filter[2]);
-            }
-        }
-        return $connection;
-    }
-
-    public function orderBy($connection, $orderby){
-        if(count($orderby) === 2){
-            $connection->orderBy($orderby[0],$orderby[1]);
-        }
-        return $connection;
-    }
-
     /**
-     * @param  Request  $request
+     * @param Request $request
      * @return Response
      */
-    public function select(Request $request){
+    public function select(Request $request)
+    {
         $validation = $this->validate($request, [
             'filter' => 'array|required',
             'orderby' => 'array|required'
         ]);
 
         $connection = DB::table('todos')
-            ->where('UID','=',$request->user()->getAuthIdentifier());
+            ->where('UID', '=', $request->user()->getAuthIdentifier());
 
         $filter = $request->input('filter');
         $orderby = $request->input('orderby');
 
-        $connection = $this->filter($connection,$filter);
-        $connection = $this->orderBy($connection,$orderby);
+        $connection = $this->filter($connection, $filter);
+        $connection = $this->orderBy($connection, $orderby);
 
         $todos = $connection->get();
 
-        $this->addResult('todos',$todos);
-        $this->addMessage('success','All your Todos.');
+        $this->addResult('todos', $todos);
+        $this->addMessage('success', 'All your Todos.');
 
         return $this->getResponse();
     }
 
     /**
-     * @param  Request  $request
+     * @param $connection
+     * @param $filters
+     * @return mixed
+     */
+    public function filter($connection, $filters)
+    {
+        foreach($filters as $key => $filter) {
+            if(count($filter) === 3) {
+                $connection->where($filter[0], $filter[1], $filter[2]);
+            }
+        }
+        return $connection;
+    }
+
+    /**
+     * @param $connection
+     * @param $orderby
+     * @return mixed
+     */
+    public function orderBy($connection, $orderby)
+    {
+        if(count($orderby) === 2) {
+            $connection->orderBy($orderby[0], $orderby[1]);
+        }
+        return $connection;
+    }
+
+    /**
+     * @param Request $request
      * @return Response
      */
-    public function view(Request $request){
+    public function view(Request $request)
+    {
         $validation = $this->validate($request, [
             'id' => 'bail|required|integer'
         ]);
@@ -64,27 +82,28 @@ class TodoController extends Controller
         $id = $request->input('id');
 
         $todo = DB::table('todos')
-            ->where('UID','=',$request->user()->getAuthIdentifier())
-            ->where('id','=',$id);
+            ->where('UID', '=', $request->user()->getAuthIdentifier())
+            ->where('id', '=', $id);
 
         $count = $todo->count();
 
-        if($count === 1){
-            $this->addResult('todo',$todo->first());
-            $this->addMessage('success','Your Todo.');
+        if($count === 1) {
+            $this->addResult('todo', $todo->first());
+            $this->addMessage('success', 'Your Todo.');
         }
-        else{
-            $this->addMessage('success','Todo doesnt exists.');
+        else {
+            $this->addMessage('success', 'Todo doesnt exists.');
         }
 
         return $this->getResponse();
     }
 
     /**
-     * @param  Request  $request
+     * @param Request $request
      * @return Response
      */
-    public function add(Request $request){
+    public function add(Request $request)
+    {
         $validation = $this->validate($request, [
             'todo' => 'bail|required|array',
             'todo.category' => 'bail|required|string',
@@ -95,31 +114,31 @@ class TodoController extends Controller
         $todo = $request->input('todo');
 
         $todos = DB::table('todos')
-            ->where('title','=',$todo['title'])
-            ->where('status','=','open')
-            ->where('category','=',$todo['category'])
-            ->where('UID','=',$request->user()->getAuthIdentifier());
+            ->where('title', '=', $todo['title'])
+            ->where('status', '=', 'open')
+            ->where('category', '=', $todo['category'])
+            ->where('UID', '=', $request->user()->getAuthIdentifier());
 
         $count = $todos->count();
         $existing = $todos->first();
 
-        if($count !== 0){
-            $this->addMessage('error','Todo already exists('.$existing->id.').');
+        if($count !== 0) {
+            $this->addMessage('error', 'Todo already exists(' . $existing->id . ').');
         }
-        else{
-            $result =  DB::table('todos')
+        else {
+            $result = DB::table('todos')
                 ->insert([
-                    'title'=>$todo['title'],
-                    'category'=>$todo['category'],
-                    'prio'=> $todo['prio'],
-                    'UID' =>$request->user()->getAuthIdentifier()
+                    'title' => $todo['title'],
+                    'category' => $todo['category'],
+                    'prio' => $todo['prio'],
+                    'UID' => $request->user()->getAuthIdentifier()
                 ]);
 
-            if($result){
-                $this->addMessage('success','Todo added successfull.');
+            if($result) {
+                $this->addMessage('success', 'Todo added successfull.');
             }
-            else{
-                $this->addMessage('warning','Upps da ist wohl was schief gelaufen.');
+            else {
+                $this->addMessage('warning', 'Upps da ist wohl was schief gelaufen.');
             }
         }
 
@@ -127,10 +146,11 @@ class TodoController extends Controller
     }
 
     /**
-     * @param  Request  $request
+     * @param Request $request
      * @return Response
      */
-    public function edit(Request $request){
+    public function edit(Request $request)
+    {
         $validation = $this->validate($request, [
             'todo' => 'bail|required|array',
             'todo.id' => 'bail|required|integer',
@@ -142,30 +162,31 @@ class TodoController extends Controller
 
         $todo = $request->input('todo');
 
-        $result =  DB::table('todos')
-            ->where('id','=',$todo['id'])
+        $result = DB::table('todos')
+            ->where('id', '=', $todo['id'])
             ->update([
-                'title'=>$todo['title'],
-                'status'=>$todo['status'],
-                'category'=>$todo['category'],
-                'prio'=> $todo['prio']
+                'title' => $todo['title'],
+                'status' => $todo['status'],
+                'category' => $todo['category'],
+                'prio' => $todo['prio']
             ]);
 
-        if($result){
-            $this->addMessage('success','Todo updated successful');
+        if($result) {
+            $this->addMessage('success', 'Todo updated successful');
         }
-        else{
-            $this->addMessage('warning','Upps da ist wohl was schief gelaufen.');
+        else {
+            $this->addMessage('warning', 'Upps da ist wohl was schief gelaufen.');
         }
 
         return $this->getResponse();
     }
 
     /**
-     * @param  Request  $request
+     * @param Request $request
      * @return Response
      */
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
         $validation = $this->validate($request, [
             'id' => 'bail|required|integer'
         ]);
@@ -173,22 +194,22 @@ class TodoController extends Controller
         $id = $request->input('id');
 
         $todo = DB::table('todos')
-            ->where('id','=',$id)
-            ->where('UID','=',$request->user()->getAuthIdentifier());
+            ->where('id', '=', $id)
+            ->where('UID', '=', $request->user()->getAuthIdentifier());
 
         $count = $todo->count();
 
-        if($count === 1){
+        if($count === 1) {
             $result = $todo->delete();
-            if($result){
-                $this->addMessage('success','Todo successful removed.');
+            if($result) {
+                $this->addMessage('success', 'Todo successful removed.');
             }
-            else{
-                $this->addMessage('warning','Upps something went wrong.');
+            else {
+                $this->addMessage('warning', 'Upps something went wrong.');
             }
         }
-        else{
-            $this->addMessage('warning','Todo doesnt exists.');
+        else {
+            $this->addMessage('warning', 'Todo doesnt exists.');
         }
 
         return $this->getResponse();
